942d30b9ce1ad33d67d70be93fb56e9426fd3fab
DanielNevesPP <85492821+DanielNevesPP@users.noreply.github.com>
2023-05-29 18:53:28 -0300

    Update counters.js

--- components/counters.js
+++ components/counters.js
@@ -6,48 +6,9 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-/**
- * @file
- *
- * Summary.
- * <p>A component to hold all counters.</p>
- *
- * This is a controlled component, because it does not have any state and
- * it simply receive data and add methods to modify the data via props.
- *
- * @author {@link https://codewithmosh.com Mosh Hamedani}
- * @author Paulo Roma
- * @since 08/10/2021
- * @see <a href="../src/components/counters.jsx">source</a>
- * @see <a href="../components/counters.js">source Babel</a>
- * @see <a href="/cwdc/14-react/counter/counter.html">link</a>
- */
-
-/**
- * <p>Keeps a reset button and a set of buttons for each counter.</p>
- *
- * @extends {React.Component}
- * @see <a href="../doc-counter/Counter.html"> Counter component </a>
- */
 var Counters = function (_React$Component) {
     _inherits(Counters, _React$Component);
 
-    /**
-     * Class components always need to call the base constructor with props.
-     * Moreover, ES6 classes need to call super in case they are subclasses.
-     * Thus, if you wish to use it in the constructor, you need to pass it to super().
-     *
-     * In case we omit it, we can always find props available inside render function.
-     * @param {Object} props component input.
-     * @param props.counters {Array<Object<id:Number,value:Number>>} array of counter objects.
-     * @param props.onReset {Function} callback to reset all counters.
-     * @param props.onIncrement {Function} callback to increment a counter.
-     * @param props.onDecrement {Function} callback to decrement a counter.
-     * @param props.onDelete {Function} callback to delete a counter.
-     * @extends {React.Component<Props>}
-     * @see https://reactjs.org/docs/react-component.html
-     * @see https://www.digitalocean.com/community/tutorials/react-constructors-with-react-components
-     */
     function Counters(props) {
         _classCallCheck(this, Counters);
 
@@ -56,17 +17,6 @@ var Counters = function (_React$Component) {
         console.log("Counters constructor: props", props);
         return _this;
     }
-
-    /**
-     * <p>Using a special prop called children to pass something between the opening and closing tag
-     * of an element. Counter could be self closed, but here we pass an &lt;h6&gt; tag.</p>
-     *
-     * This is useful when using dialog boxes to allow the consumer of that component
-     * to pass content to be rendered on the dialog box.
-     *
-     * @return {HTMLDivElement} a reset button plus a number of Counter components.
-     */
-
 
     _createClass(Counters, [{
         key: "render",
