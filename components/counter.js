909345fc77c1e7f7d46967f006ad45685b652535
DanielNevesPP <85492821+DanielNevesPP@users.noreply.github.com>
2023-05-29 18:53:04 -0300

    Update counter.js

--- components/counter.js
+++ components/counter.js
@@ -6,48 +6,9 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-/**
- * @file
- *
- * Summary.
- * <p>A component to display and modify a counter.</p>
- *
- * Counter raises the events that are handled by the Counters component.
- *
- * @author {@link https://codewithmosh.com|Mosh Hamedani}
- * @author Paulo Roma
- * @since 08/10/2021
- * @see <a href="../src/components/counter.jsx">source</a>
- * @see <a href="../components/counter.js">source Babel</a>
- * @see <a href="/cwdc/14-react/counter/counter.html">link</a>
- */
-
-/**
- * <p>Creates a row of buttons for a counter.</p>
- *
- * Also define some life cycle hooks.
- * @extends {React.Component}
- * @see <img src="../row.png" width="256">
- */
 var Counter = function (_React$Component) {
     _inherits(Counter, _React$Component);
 
-    /**
-     * Class components always need to call the base constructor with props.
-     * Moreover, ES6 classes need to call super in case they are subclasses.
-     * Thus, if you wish to use it in the constructor, you need to pass it to super().
-     *
-     * In case we omit it, we can always find props available inside render function.
-     * @param {Object} props component input.
-     * @param props.key {Number} counter id.
-     * @param props.onDelete {Function} callback to delete a counter.
-     * @param props.onIncrement {Function} callback to increment a counter.
-     * @param props.onDecrement {Function} callback to decrement a counter.
-     * @param props.counter {Object<id:Number,value:Number>} counter object.
-     * @extends {React.Component<Props>}
-     * @see https://reactjs.org/docs/react-component.html
-     * @see https://www.digitalocean.com/community/tutorials/react-constructors-with-react-components
-     */
     function Counter(props) {
         _classCallCheck(this, Counter);
 
@@ -56,18 +17,6 @@ var Counter = function (_React$Component) {
         console.log("Counter constructor: props", props);
         return _this;
     }
-
-    /**
-     * <p>Called when a component is updated. </p>
-     * Remember that setState is asynchronous.<br>
-     * Furthermore, there is no state in this class.<br>
-     * As a consequence, prevState is always null.
-     *
-     * @param {Object} prevProps previous props object.
-     * @param {Object} prevState previous state object.
-     * @see https://dev.to/cesareferrari/how-to-use-componentdidupdate-in-react-30en
-     */
-
 
     _createClass(Counter, [{
         key: "componentDidUpdate",
@@ -79,28 +28,12 @@ var Counter = function (_React$Component) {
                 console.log("Props: " + this.props.counter.value);
             }
         }
-
-        /**
-         * Called when the component is destroyed.
-         */
-
+        
     }, {
         key: "componentWillUnmount",
         value: function componentWillUnmount() {
             console.log("Counter - Unmount");
         }
-
-        /**
-         * Use bootstrap's <a href="/cwdc/5-bootstrap/5.3.html">grid system</a>
-         * to build layouts: one row and two columns.
-         *
-         * @return {HTMLDivElement} a badge, plus an increment, a decrement, and a delete button.
-         * @see https://getbootstrap.com/docs/4.6/layout/grid/
-         * @see https://getbootstrap.com/docs/4.6/utilities/spacing/
-         * @see https://www.bitdegree.org/learn/horizontal-grid
-         * @see https://icons.getbootstrap.com/icons/trash/
-         * @see https://fontawesome.com/v4/icons/
-         */
 
     }, {
         key: "render",
@@ -170,12 +103,6 @@ var Counter = function (_React$Component) {
             );
         }
 
-        /**
-         * Returns the color for this counter badge: yellow for 'Zero', or blue otherwise.
-         *
-         * @returns {String} the class for setting the color of this counter badge.
-         */
-
     }, {
         key: "getBadgeClasses",
         value: function getBadgeClasses() {
@@ -183,12 +110,6 @@ var Counter = function (_React$Component) {
             classes += this.props.counter.value === 0 ? "warning" : "primary";
             return classes;
         }
-
-        /**
-         * Format the value of this counter as 'Zero' or its numerical value.
-         *
-         * @returns {String|Number} the value of this counter.
-         */
 
     }, {
         key: "formatCount",
